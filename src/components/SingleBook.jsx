b0e3af107b4085b6c36344ef62c3231cfe363cd5
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-09-30 15:24:52 +0200

    Update SingleBook.jsx

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,16 +1,32 @@
+import {Component} from "react"
 import Card from "react-bootstrap/Card"
 import MyBadge from "./MyBadge"
 
 
-const SingleBook = (obj) => (
-    <Card style={{ width: '15rem' }}>
-        <Card.Img variant="top" src={obj.obj.img} />
-        <Card.Body>
-            <Card.Title>{obj.obj.title}</Card.Title>
-            <MyBadge color="primary" message={obj.obj.price}/>
-        </Card.Body>
-    </Card>
-)
+class SingleBook extends Component {
+
+    /* state = {
+        selected : false
+    }
+
+    ifSelected() {
+
+    } */
+
+    render() {
+        return(
+            <>
+                <Card /* onClick={() => ifSelected()}  */ style={{ width: '15rem' }}>
+                    <Card.Img variant="top" src={this.props.obj.img} />
+                    <Card.Body>
+                        <Card.Title>{this.props.obj.title}</Card.Title>
+                        <MyBadge color="primary" message={this.props.obj.price}/>
+                    </Card.Body>
+                </Card>
+            </>
+        )
+    }
+}
 
 
 export default SingleBook
